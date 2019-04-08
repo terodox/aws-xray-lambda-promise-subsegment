14d1c0df2c06b8b756f155658f3e10f6f6f2edcf
terodox <terodox@gmail.com>
2019-04-07 21:51:40 -0400

    Prove out ability to nest segments

--- index.v2.js
+++ index.v2.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const XRay = require("aws-xray-sdk");
+const XRay = require("aws-xray-sdk-core");
 
 function addMetadata(subSegment, metadata) {
     if(metadata) {
@@ -22,40 +22,45 @@ function addAnnotations(subSegment, annotations) {
     }
 }
 
-module.exports.addPromiseSegment = function (segmentName, inputPromise, metadata, annotations) {
-    if (!process.env.LAMBDA_TASK_ROOT) {
+module.exports.addPromiseSegment = async function ({
+    __ignoreLambdaSafetyCheck,
+    annotations,
+    parentSegment,
+    promiseFactory,
+    metadata,
+    segmentName,
+}) {
+    if (!process.env.LAMBDA_TASK_ROOT && !__ignoreLambdaSafetyCheck) {
         console.warn('WARNING: Skipping adding subsegment because we are not executing inside of aws lambda');
-        return inputPromise;
+        return promiseFactory();
     }
 
     return new Promise((resolve, reject) => {
         try {
             XRay.captureAsyncFunc(segmentName, (subSegment) => {
                 try {
-console.log('subSegment', subSegment);
                     if (!subSegment) {
-                        return inputPromise
+                        return promiseFactory()
                             .then(val => resolve(val))
                             .catch(err => reject(err));
                     } else {
                         addMetadata(subSegment, metadata);
                         addAnnotations(subSegment, annotations);
 
-                        inputPromise
+                        promiseFactory(subSegment)
                             .then(val => {
                                 resolve(val);
                                 subSegment.close();
                             })
                             .catch(err => {
                                 reject(err);
-                                subSegment.addError(err);
-                                subSegment.close();
+                                subSegment.close(err);
                             });
                     }
                 } catch(err) {
                     reject(err);
                 }
-            });
+            }, parentSegment);
         } catch (err) {
             reject(err);
         }
